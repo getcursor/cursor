f450769a8f4879497d9d38df776b2cdfd7b73c8f
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2023-03-24 14:08:50 +0200

    Fix tab split on reorder if tab is not inside the content window

--- src/components/pane.tsx
+++ src/components/pane.tsx
@@ -50,6 +50,8 @@ export function Pane({ paneId }: { paneId: number }) {
     const draggingTabId = useAppSelector(getDraggingTabId)
     const paneDiv = React.useRef<HTMLDivElement>(null)
     const [hoverState, setHoverState] = useState(HoverState.None)
+    const [initialSelectPaneMousePosition, setInitialSelectPaneMousePosition] =
+        useState<{ x: number; y: number } | null>(null)
 
     let paneHoverClassName = 'pane__hover'
     if (hoverState == HoverState.Left) paneHoverClassName += ' pane__hover_left'
@@ -67,11 +69,15 @@ export function Pane({ paneId }: { paneId: number }) {
         let vertMargin = rect.height / 4
         let xInDiv = x - rect.left
         let yInDiv = y - rect.top
-
-        if (xInDiv < horizMargin) return HoverState.Left
-        if (xInDiv > rect.width - horizMargin) return HoverState.Right
-        if (yInDiv < vertMargin) return HoverState.Top
-        if (yInDiv > rect.height - vertMargin) return HoverState.Bottom
+        // take into account the title-bar height and the position of the mouse when start dragging the tab
+        const titleBarHeightPx = 47;
+        const yCalculated = (y - (initialSelectPaneMousePosition?.y as number) - titleBarHeightPx)        
+        if (yCalculated > 0) {
+            if (xInDiv < horizMargin) return HoverState.Left
+            if (xInDiv > rect.width - horizMargin) return HoverState.Right
+            if (yInDiv < vertMargin) return HoverState.Top
+            if (yInDiv > rect.height - vertMargin) return HoverState.Bottom
+        }
         return HoverState.Full
     }
 
@@ -126,6 +132,17 @@ export function Pane({ paneId }: { paneId: number }) {
             />
             <div
                 className="pane__content"
+                onDragStart={(event) => {
+                    if (initialSelectPaneMousePosition === null) {
+                        setInitialSelectPaneMousePosition({
+                            x: event.clientX,
+                            y: event.clientY,
+                        })
+                    }
+                }}
+                onDragEnd={() => {
+                    setInitialSelectPaneMousePosition(null)
+                }}
                 onDragOver={onDragCallback}
                 onDrop={onDropCallback}
                 onDragLeave={onDragLeave}
