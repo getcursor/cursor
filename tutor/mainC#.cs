c1d3cc50ff6825cd0dde7fccb92af6470b35d6af
Mileta Avramovic <92308650+MiletaA@users.noreply.github.com>
2023-03-28 00:00:34 +0200

    More programming languages examples.

---
+++ tutor/mainC#.cs
@@ -0,0 +1,6 @@
+// Welcome to Cursor
+//
+// 1. Try generating with command K on a new line. Ask for a C# implementation of a binary search tree.
+// 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+// 3. Try selecting some code and hitting edit. Ask the bot to add a method for finding the lowest common ancestor of two nodes.
+// 4. To try out Cursor on your own projects, go to the file menu (top left) and open a folder.
---
+++ tutor/mainRust.rs
@@ -0,0 +1,6 @@
+// Welcome to Cursor
+//
+// 1. Try generating with command K on a new line. Ask for a Rust implementation of a concurrent hash map.
+// 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+// 3. Try selecting some code and hitting edit. Ask the bot to add a method for safely removing a key-value pair from the hash map.
+// 4. To try out Cursor on your own projects, go to the file menu (top left) and open a folder.
