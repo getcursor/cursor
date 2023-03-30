b85efe2a98e18e386ea2c1d731b7733c3fc0ddac
Michael Truell <mntruell@gmail.com>
2023-03-29 19:25:46 -0700

    done with messages

--- src/components/errors.tsx
+++ src/components/errors.tsx
@@ -4,10 +4,31 @@ import {getShowErrors, getError} from '../features/selectors';
 import { faClose } from '@fortawesome/pro-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Modal from 'react-modal'
-import { NoAuthRateLimitError, NotLoggedInError, OpenAIError } from '../utils';
+import { NoAuthGlobalOldRateLimitError, NotLoggedInError, OpenAIError } from '../utils';
 import { CursorLogin, OpenAILoginPanel } from './settingsPane';
 
 const customStyles = {
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        alignItems: 'center',
+        zIndex: 10000,
+    },
+    content: {
+        padding: 'none',
+        top: '150px',
+        bottom: 'none',
+        background: 'none',
+        border: 'none',
+        width: 'auto',
+        height: 'auto',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        maxWidth: '600px',
+    },
+}
+
+const loginStyles = {
     overlay: {
         backgroundColor: 'rgba(0, 0, 0, 0.1)',
         display: 'flex',
@@ -32,12 +53,10 @@ const customStyles = {
 
 export function ErrorPopup() {
     const showError = useAppSelector(getShowErrors)
-    const error = new NotLoggedInError()
+    const error = useAppSelector(getError)
     const dispatch = useAppDispatch()
 
-    console.log('waht')
-
-    if (false && error == null) {
+    if (error == null) {
         return (
             <Modal
                 isOpen={showError}
@@ -66,14 +85,14 @@ export function ErrorPopup() {
                 </div>
             </Modal>
         )
-    } else if (true || error instanceof NotLoggedInError) {
+    } else if (error instanceof NotLoggedInError) {
         return (
             <Modal
-                isOpen={true || showError}
+                isOpen={showError}
                 onRequestClose={() => {
                     dispatch(closeError())
                 }}
-                style={customStyles}
+                style={loginStyles}
             >
                 <div className="errorPopup">
                     <div className="errorPopup__title">
@@ -105,34 +124,72 @@ export function ErrorPopup() {
                 </div>
             </Modal>
         )
+    } else if (error instanceof NoAuthGlobalOldRateLimitError) {
+        return (
+            <Modal
+                isOpen={true || showError}
+                onRequestClose={() => {
+                    dispatch(closeError())
+                }}
+                style={loginStyles}
+            >
+                <div className="errorPopup">
+                    <div className="errorPopup__title">
+                        <div className="errorPopup__title_text">
+                        </div>
+                        <div
+                            className="errorPopup__title_close"
+                            onClick={() => dispatch(closeError())}
+                        >
+                            <FontAwesomeIcon icon={faClose} />
+                        </div>
+                    </div>
+                        <div className="signup__body">
+                           <div className="signup__title">Free tier limit exceeded</div>
+                            <div className="signup__module">
+                               <div className="signup__subtitle">If you've enjoyed using Cursor, please consider subscribing to one of our paid plans</div>
+                               <div className="signup__signup_button">Upgrade</div>
+                            </div>
+                            <div className="signup__module signup__last_module">
+                               <div className="signup__subtitle">Or enter your OpenAI API key to continue using the AI features at-cost</div>
+                                <OpenAILoginPanel onSubmit={
+                                    () => {
+                                        dispatch(closeError())
+                                    }
+                                } />
+                            </div>
+                        </div>
+                </div>
+            </Modal>
+        )
     } else {
-        // return (
-        //     <Modal
-        //         isOpen={showError}
-        //         onRequestClose={() => {
-        //             dispatch(closeError())
-        //         }}
-        //         style={customStyles}
-        //     >
-        //         <div className="errorPopup">
-        //             <div className="errorPopup__title">
-        //                 <div className="errorPopup__title_text">
-        //                     {error.title}
-        //                 </div>
-        //                 <div
-        //                     className="errorPopup__title_close"
-        //                     onClick={() => dispatch(closeError())}
-        //                 >
-        //                     <FontAwesomeIcon icon={faClose} />
-        //                 </div>
-        //             </div>
-        //             <div className="errorPopup__body">
-        //                 {error.message}
-        //                 <br />
-        //             </div>
-        //         </div>
-        //     </Modal>
-        // )
+        return (
+            <Modal
+                isOpen={true || showError}
+                onRequestClose={() => {
+                    dispatch(closeError())
+                }}
+                style={customStyles}
+            >
+                <div className="errorPopup">
+                    <div className="errorPopup__title">
+                        <div className="errorPopup__title_text">
+{/*                             {error.title} */}
+                        </div>
+                        <div
+                            className="errorPopup__title_close"
+                            onClick={() => dispatch(closeError())}
+                        >
+                            <FontAwesomeIcon icon={faClose} />
+                        </div>
+                    </div>
+                    <div className="errorPopup__body">
+{/*                         {error.message} */}
+                        <br />
+                    </div>
+                </div>
+            </Modal>
+        )
     }
         
         
