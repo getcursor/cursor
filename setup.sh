0f2fe04239f66fc4d7004b07850b0b4644f8b03e
Peng Gu <penggu@gmail.com>
2023-04-03 10:10:25 -0700

    allow ./setup.sh to fail gracefully

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+function command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+if ! command_exists wget; then
+    echo "wget is not installed. Please install wget to use this script."
+    exit 1
+fi
+
+if ! command_exists unzip; then
+    echo "unzip is not installed. Please install unzip to use this script."
+    exit 1
+fi
+
 wget https://cursor.so/resources.zip
 wget https://cursor.so/lsp.zip
 
