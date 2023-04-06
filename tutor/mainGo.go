49b0ee13f9e31738e99a85bc2d23591b97238a26
Mileta Avramovic <92308650+MiletaA@users.noreply.github.com>
2023-04-06 06:58:39 +0200

    More programming languages examples. (#312)

---
+++ tutor/mainGo.go
@@ -0,0 +1,6 @@
+// Welcome to Cursor
+//
+// 1. Try generating with command K on a new line. Ask for a Go implementation of a simple HTTP server.
+// 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+// 3. Try selecting some code and hitting edit. Ask the bot to add a middleware for logging requests.
+// 4. To try out Cursor on your own projects, go to the file menu (top left) and open a folder.
---
+++ tutor/mainPhp.php
@@ -0,0 +1,6 @@
+/* Welcome to Cursor
+
+1. Try generating with command K on a new line. Ask for a PHP script that connects to a MySQL database.
+2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+3. Try selecting some code and hitting edit. Ask the bot to add a function to retrieve data from a specific table.
+4. To try out Cursor on your own projects, go to the file menu (top left) and open a folder. */
